9e886146828654b74089fe23b54c494bfb661794
IvanTC23 <i.telcug99@gmail.com>
2026-03-03 09:58:20 +0100

    Se ha añadido un favicon a la página

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ ProcesarNuevaTarea($jsonPath, $tareas);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="shortcut icon" href="https://icons8.com/icon/63262/checkmark" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
     <title>Gitflow - PPS</title>
 </head>
